0df7e4d97f6fe09bb5c4ee89e91f7503d58a2751
Colby Pate <cbpate08@gmail.com>
2022-08-29 14:54:38 -0500

    Initial Commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FarenheitToCelsius(68);
+            var farenheit = TempConverter.CelsiusToFarenheit(20);
+            Console.WriteLine($"Celsius : {celsius}");
+            Console.WriteLine($"Farenheit : {farenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheiht)
+        {
+            return ((farenheiht - 32) / 1.8);
+        }
+
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            return ((celsius * 9) / 5) + 32;
+        }
+    }
+
+
+}
